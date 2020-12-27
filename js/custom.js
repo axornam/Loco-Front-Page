fba00511ad41fb4a0c699159c6f5da9b08152023
Richard Axornam <axornam.richard@gmail.com>
2020-12-27 15:56:20 +0000

    Add nav-item highlight on scroll

--- js/custom.js
+++ js/custom.js
@@ -59,3 +59,46 @@ $(document).ready(function () {
     }
   });
 });
+
+// Active menu Item on Click
+$(document).ready(function () {
+  "use strict";
+
+  $(".navbar-nav li a").click(function () {
+    "use strict";
+    $(".navbar-nav li a").parent().removeClass("active");
+
+    $(this).parent().addClass("active");
+  });
+});
+
+// Highlight Menu Item on Scroll
+$(document).ready(function () {
+  "use strict";
+
+  $(window).scroll(function () {
+    "use strict";
+
+    $("section").each(function () {
+      "use strict";
+
+      var bb = $(this).attr("id");
+      console.log(bb);
+      var hei = $(this).outerHeight();
+      var grttop = $(this).offset().top - 70;
+
+      if (
+        $(window).scrollTop() > grttop &&
+        $(window).scrollTop() < grttop + hei
+      ) {
+        $(".navbar-nav li a[href='#" + bb + "']")
+          .parent()
+          .addClass("active");
+      } else {
+        $(".navbar-nav li a[href='#" + bb + "']")
+          .parent()
+          .removeClass("active");
+      }
+    });
+  });
+});
